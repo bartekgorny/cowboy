cd83171c4a11fc053f1b0e4f6c8443b1fb3a2afd
Rafał Słota <roxeon@gmail.com>
2014-09-19 23:46:56 +0200

    fix get_int

--- c_src/websocket_utils.c
+++ c_src/websocket_utils.c
@@ -22,28 +22,63 @@
 #define BADARG enif_make_badarg(env)
 
 
+// Check if local arch endiannes is little-endian
+int is_littlendian() {
+    union {
+        uint16_t num;
+        char     buff[2];
+    } x;
+
+    x.num = 0x01;
+    return x.buff[0] == 0x01;
+}
+
+int64_t hton64(int64_t x) {
+    if(!is_littlendian()) {
+        return x;    
+    }
+    
+    union {
+        int64_t num;
+        char    buff[8];
+    } rval, lval;
+
+    lval.num = x;
+
+    for(int i = 0; i < 8; ++i) {
+        rval.buff[i] = lval.buff[ 7 - i ];
+    }
+
+    return rval.num;
+}
+
+
+int64_t ntoh64(int64_t x) {
+    return hton64(x);
+}
+
 static ERL_NIF_TERM websocket_unmask(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     ErlNifBinary bin;
     ERL_NIF_TERM unmasked;
     union {
-        int32_t         num;
-        unsigned char   bin[4];
+        ErlNifSInt64    num;
+        unsigned char   bin[8];
     } mask_key;
 
     if(!enif_inspect_binary(env, argv[0], &bin))
         return BADARG;
 
-    if(!enif_get_int(env, argv[1], &mask_key.num))
+    if(!enif_get_int64(env, argv[1], &mask_key.num))
         return BADARG;
 
-    mask_key.num = htonl(mask_key.num);
+    mask_key.num = hton64(mask_key.num);
 
     char *tmp = (char *) enif_make_new_binary(env, bin.size, &unmasked);
 
     for(int i = 0; i < bin.size; ++i)
     {
-        tmp[i] = bin.data[i] ^ mask_key.bin[i % 4];
+        tmp[i] = bin.data[i] ^ mask_key.bin[4 + (i % 4)];
     }
 
     return unmasked;
